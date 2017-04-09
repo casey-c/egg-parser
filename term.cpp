68b841276ea9396bb1ac3affef310c0b99d7a58e
casey <casey-c@users.noreply.github.com>
2017-04-09 14:08:28 -0400

    Added -d flag for term to check if the input string is already standardized.

--- term.cpp
+++ term.cpp
@@ -4,10 +4,12 @@
 
 int main( int argc, char* argv[] )
 {
-    if ( argc != 2)
+    if ( argc < 2)
     {
         std::cout << "Usage: run \"./a.out 21AB\" where 21AB is the string you"
-                  << " wish to standardize.\n";
+                  << " wish to standardize.\nFlag -d will perform a diff on"
+                  << " the output and input string to see if it is already"
+                  << " standardized.\n";
         return -1;
     }
 
@@ -20,5 +22,33 @@ int main( int argc, char* argv[] )
     }
 
     Node::standardize( node );
+    std::string output = node->toString();
+
+    // Extra flags
+    if (argc > 2)
+    {
+        std::string flag( argv[2] );
+
+        // Check if the input and output string are already standardized
+        if (flag == "-d")
+        {
+            if (input == output)
+            {
+                std::cout << "Already standard:" << std::endl;
+                std::cout << "In:  " << input << std::endl;
+                std::cout << "Out: " << output << std::endl;
+                return 0;
+            }
+            else
+            {
+                std::cout << "Not already standardized." << std::endl;
+                std::cout << "In:  " << input << std::endl;
+                std::cout << "Out: " << output << std::endl;
+                return -1;
+            }
+
+        }
+    }
+
     std::cout << node->toString() << std::endl;
 }
